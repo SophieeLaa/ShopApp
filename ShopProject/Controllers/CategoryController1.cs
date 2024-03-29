f4795e8c6dc60c96a143379662f5074d8aeb301a
UserSophie <UserSophie@DESKTOP-J6A1FK4>
2024-03-29 06:34:09 -0700

    Add project files.

---
+++ ShopProject/Controllers/CategoryController1.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopProject.Data;
+using ShopProject.Models;
+
+namespace ShopProject.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext  db)
+        {
+            _db = db;   
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+    }
+}
