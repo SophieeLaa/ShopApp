12271e1f39c1b6edb286f8b326722cd2952074f6
sophie 888 <sophie88998899@gmail.com>
2024-04-01 05:30:46 -0700

    Adding validation

--- ShopProject/Models/Category.cs
+++ ShopProject/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace ShopProject.Models
 {
@@ -7,7 +8,12 @@ namespace ShopProject.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [MaxLength(30)]
+        [DisplayName("Category Name")]
         public string Name { get; set; }
+
+        [DisplayName("Dispay Order")]
+        [Range(1,100, ErrorMessage="Dispkay Order must be between 1-100")]
         public int DisplayOrder { get; set; }
     }
 }
