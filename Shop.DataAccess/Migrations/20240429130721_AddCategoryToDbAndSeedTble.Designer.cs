860e39982f85a0e037ee8a5ebf2496daedd19b2c
sophie 888 <sophie88998899@gmail.com>
2024-04-29 06:45:57 -0700

    New migrations

--- Shop.DataAccess/Migrations/20240429130721_AddCategoryToDbAndSeedTble.Designer.cs
+++ Shop.DataAccess/Migrations/20240429130721_AddCategoryToDbAndSeedTble.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ShopProject.Data;
 
@@ -10,9 +11,11 @@ using ShopProject.Data;
 namespace ShopProject.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240429130721_AddCategoryToDbAndSeedTble")]
+    partial class AddCategoryToDbAndSeedTble
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
